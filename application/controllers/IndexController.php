01516b4699b866402ecb8ddf1d7a6d6990a487ea
Marcin <kumeek@gmail.com>
2012-01-13 12:11:51 +0100

    added Zend and Doctrine to library, changed Models to Doctrine generated ones

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -12,8 +12,49 @@ class IndexController extends Zend_Controller_Action
     {
         // action body
         $this->view->currentDateAndTime = date('d-m-Y, H:i:s', time());
+
+    }
+
+    public function updateAction()
+    {
+        // action body
+//        $user_mapper = new Application_Model_UserMapper();
+//        $user = $user_mapper->getUserById(1);
+//        $user->email = 'new_email@test.local';
+//        $user_mapper->save($user);
+
+        $user = Doctrine_Core::getTable('Model_User')->findOneByEmailAndPassword('bez.niczego@gmail.com', 'bezniczego');
+        $user->password = 'test';
+        $user->save();
+    }
+
+    public function addAction()
+    {
+        // action body
+
+        //        adding user with OldUser model
+        //        $user = new Application_Model_User();
+        //        $user->email = 'nikko@test.local';
+        //        $user->salt = sha1(time());
+        //        $user->password = 'test';
+        //        $user->date_created = date('Y-m-d H:i:s');
+        //
+        //        $user_mapper = new Application_Model_UserMapper();
+        //        $user_mapper->save($user);
+
+        // using Doctrine
+        $user = new Model_User();
+        $user->email = 'bez.niczego@gmail.com';
+        $user->password = 'bezniczego';
+        $user->salt = sha1(time());
+        $user->date_created = date('Y-m-d H:i:s');
+        $user->save();
     }
 
 
 }
+
+
+
+
 
